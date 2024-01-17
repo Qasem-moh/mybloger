dcdd3320cffb509579fb53f87e85b7dba3460aef
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-17 15:53:23 +0300

    blog

--- src/Components/UseFetch.js
+++ src/Components/UseFetch.js
@@ -7,8 +7,31 @@ const useFetch=(url)=>{
 
     useEffect(()=>{
         const abortCont=new AbortController();
-    })
-
+        setTimeout(() => {
+            fetch(url, {signal: abortCont.signal})
+                .then(res => {
+                    if (!res.ok) {
+                        throw Error("Could not fetch the data from the resource")
+                    }
+                    return res.json();
+                })
+                .then(data => {
+                    setIsPending(false)
+                    setData(data)
+                    seterror(null)
+                })
+                .catch(err => {
+                    if (err.name === "AbortError") {
+                        console.log("fetch aborting")
+                    } else {
+                        setIsPending(false)
+                        seterror(err.message)
+                    }
+                })
+        }, 1000)
+        return () => abortCont.abort()
+    }, [url])
+    return {data, isPendig, error}
 }
 
 export default useFetch
