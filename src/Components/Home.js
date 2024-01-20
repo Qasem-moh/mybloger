4435e45764acb6fbf0d60ef713332ff1a508d896
Qasem Mohammd <75634309+Qasem-moh@users.noreply.github.com>
2024-01-20 18:13:11 +0300

    blog

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -5,7 +5,7 @@ import BlogList from "./BlogList";
 
 function Home() {
     // const {data,isPending,error}=useFetch('https://qasemmo.onrender.com/getAllArticles')
-    const {data,isPending,error}=useFetch('https://newsapi.org/v2/everything?q=Gaza&from=2024-01-19&to=2024-01-19&sortBy=popularity&apiKey=033f8eb6abbb4481a8d57b7c2c6c1ee0')
+    const {data,isPending,error}=useFetch('https://newsapi.org/v2/everything?q=sa&from=2024-01-19&to=2024-01-19&sortBy=popularity&apiKey=033f8eb6abbb4481a8d57b7c2c6c1ee0')
     console.log(data)
     return (
         <div className="home">
